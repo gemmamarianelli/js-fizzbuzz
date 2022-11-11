cb04be471363ed3ed5e36a2e5891e10d0cc8808d
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-11 16:04:28 +0100

    primo push

---
+++ script.js
@@ -0,0 +1,12 @@
+for(let i = 1; i<100; i++){
+    console.log(i);
+    if (i ){
+         console.log("FizzBuzz");
+    }else if (i ) {
+    console.log("Fizz");
+    }else if (i % ){
+     console.log("Buzz");
+    }else {
+    console.log(i);
+    }
+}
