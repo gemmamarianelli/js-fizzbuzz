bc1aaa99e162bfe4c4d90cdd71833893928ee5dc
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-11 16:26:42 +0100

    secondo push

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-for(let i = 1; i<100; i++){
+for(let i = 1; i<101; i++){
     console.log(i);
-    if (i ){
+    if (i % 15 == 0){
          console.log("FizzBuzz");
-    }else if (i ) {
+    }else if (i % 3 == 0) {
     console.log("Fizz");
-    }else if (i % ){
+    }else if (i % 5 == 0){
      console.log("Buzz");
     }else {
     console.log(i);
